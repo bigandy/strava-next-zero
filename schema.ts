091cf3c8c2e83d6cc5fc3a820819f0347351f926
Austin Poor <45295232+a-poor@users.noreply.github.com>
2025-01-27 15:53:44 -0800

    Sets up schema, db, and docker; Installs zero

---
+++ schema.ts
@@ -0,0 +1,49 @@
+import {
+  createSchema,
+  definePermissions,
+  Row,
+  table,
+  string,
+  relationships,
+} from "@rocicorp/zero";
+
+const user = table("user")
+  .columns({
+    id: string(),
+    name: string(),
+  })
+  .primaryKey("id");
+
+const task = table("task")
+  .columns({
+    id: string(),
+    name: string(),
+    status: string(),
+    createdById: string(),
+    assignedToId: string(),
+  })
+  .primaryKey("id");
+
+const taskRelationships = relationships(task, ({ one }) => ({
+  createdBy: one({
+    sourceField: ["createdById"],
+    destSchema: user,
+    destField: ["id"],
+  }),
+  assignedTo: one({
+    sourceField: ["assignedToId"],
+    destSchema: user,
+    destField: ["id"],
+  }),
+}));
+
+export const schema = createSchema(1, {
+  tables: [user, task],
+  relationships: [taskRelationships],
+});
+
+export type Schema = typeof schema;
+export type User = Row<typeof schema.tables.user>;
+export type Task = Row<typeof schema.tables.task>;
+
+export const permissions = definePermissions(schema, () => ({}));
