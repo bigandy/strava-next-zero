04061960af61b950edd57259317f0ca21d0640bc
Andrew Hudson <ahudson@gmail.com>
2025-10-27 20:07:18 +0100

    fix: table sorting

--- components/activities-with-search.tsx
+++ components/activities-with-search.tsx
@@ -12,7 +12,6 @@ import {
 } from "@tanstack/react-table";
 import { useState } from "react";
 import { useDebounceValue } from "usehooks-ts";
-import { PaginationButtons } from "./pagination-buttons";
 import { TableHead } from "./table-head";
 import { columns } from "./utils";
 
@@ -81,7 +80,73 @@ export const ActivitiesWithSearch = () => {
 						))}
 					</tbody>
 				</table>
-				<PaginationButtons table={table} />
+				<div className="flex items-center gap-2">
+					<button
+						type="button"
+						className="border rounded p-1"
+						onClick={() => table.firstPage()}
+						disabled={!table.getCanPreviousPage()}
+					>
+						{"<<"}
+					</button>
+					<button
+						type="button"
+						className="border rounded p-1"
+						onClick={() => table.previousPage()}
+						disabled={!table.getCanPreviousPage()}
+					>
+						{"<"}
+					</button>
+					<button
+						type="button"
+						className="border rounded p-1"
+						onClick={() => table.nextPage()}
+						disabled={!table.getCanNextPage()}
+					>
+						{">"}
+					</button>
+					<button
+						type="button"
+						className="border rounded p-1"
+						onClick={() => table.lastPage()}
+						disabled={!table.getCanNextPage()}
+					>
+						{">>"}
+					</button>
+					<span className="flex items-center gap-1">
+						<div>Page</div>
+						<strong>
+							{table.getState().pagination.pageIndex + 1} of{" "}
+							{table.getPageCount().toLocaleString()}
+						</strong>
+					</span>
+					<span className="flex items-center gap-1">
+						| Go to page:
+						<input
+							type="number"
+							min="1"
+							max={table.getPageCount()}
+							defaultValue={table.getState().pagination.pageIndex + 1}
+							onChange={(e) => {
+								const page = e.target.value ? Number(e.target.value) - 1 : 0;
+								table.setPageIndex(page);
+							}}
+							className="border p-1 rounded w-16"
+						/>
+					</span>
+					<select
+						value={table.getState().pagination.pageSize}
+						onChange={(e) => {
+							table.setPageSize(Number(e.target.value));
+						}}
+					>
+						{[10, 20, 30, 40, 50].map((pageSize) => (
+							<option key={pageSize} value={pageSize}>
+								Show {pageSize}
+							</option>
+						))}
+					</select>
+				</div>
 			</div>
 		</>
 	);
