99520870b60cf09fcc5d19d33dfd23fdb2eaeb62
Andrew Hudson <ahudson@gmail.com>
2025-10-30 18:10:32 +0100

    remove commented out code

--- components/utils.tsx
+++ components/utils.tsx
@@ -55,20 +55,9 @@ export const columns = [
 		accessorKey: "type",
 		header: "Type",
 	},
-	// {
-	// 	accessorKey: "id",
-	// 	header: "Edit",
-	// 	enableSorting: false,
-	// 	cell: ({ cell }: CellProps) => {
-	// 		return (
-	// 			<Link href={`/activities/${cell.getValue()}`}>Edit Activity?</Link>
-	// 		);
-	// 	},
-	// },
 ];
 
 export const singleRowColumns = [
-	// ...columns.toSpliced(columns.length - 1),
 	...columns,
 	{
 		accessorKey: "id",
